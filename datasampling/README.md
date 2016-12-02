a66433e6139eddd27342c65f932c9c52f5ad19f0
Jayden <mischievousjayden@gmail.com>
2016-12-02 18:40:17 -0500

    init datasampling

---
+++ datasampling/README.md
@@ -0,0 +1,8 @@
+The directory is for creating sample data
+- datasampling.py: sample data
+- makefile: help to create sampled data
+
+1. create raw_data, raw_data/like, and raw_data/dislike
+2. add data under expert and inexpert directory
+3. type 'make'
+
---
+++ datasampling/datasampling.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+import sys
+from os import listdir
+from os.path import isfile, join
+
+
+def sampleData(period, inputfile, outputfile):
+    """The function samples data. 
+    Example: sampleData(3, inputfile, outputfile)
+        create outputfile with every 3rd line in inputfile
+        
+    Args:
+        period (int): sampling period
+        inputfile (string): input file name
+        outputfile (string): output file name
+
+    Returns:
+        void
+    """
+
+    with open(inputfile, 'r') as fread, open(outputfile, 'w') as fwrite:
+        for i, line in enumerate(fread):
+            if (i % period) == 0:
+                fwrite.write(line)
+
+
+def sampleDataSet(period, inputpath, outputpath, suffix):
+    file_list = [f for f in listdir(inputpath) if isfile(join(inputpath, f)) and f.endswith(suffix)]
+    for f in file_list:
+        inputfile = join(inputpath, f)
+        outputfile = join(outputpath, "sample_" + f)
+        sampleData(period, inputfile, outputfile)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        message = "Usag: datasampling.py <period> <input_path> <output_path> <suffix>"
+        sys.exit(message)
+
+    period = int(sys.argv[1])
+    inputpath = sys.argv[2]
+    outputpath = sys.argv[3]
+    suffix = sys.argv[4]
+    sampleDataSet(period, inputpath, outputpath, suffix)
+
